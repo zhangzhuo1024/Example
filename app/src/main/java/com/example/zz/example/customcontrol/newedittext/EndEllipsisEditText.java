8816aff6ce6c9b8edfe1cd5253c32abe295e6930
zhuozhang6 <zhuozhang6@iflytek.com>
2020-09-23 20:18:21 +0800

    自定义EditText，默认无焦点，键盘收起显示省略号，gravity等于left时完美适配，gravity等于right时，超长后点击光标位置不准，待优化

--- app/src/main/java/com/example/zz/example/customcontrol/newedittext/EndEllipsisEditText.java
+++ app/src/main/java/com/example/zz/example/customcontrol/newedittext/EndEllipsisEditText.java
@@ -1,11 +1,13 @@
 package com.example.zz.example.customcontrol.newedittext;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Paint;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
+import android.graphics.Rect;
+import android.text.Editable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.EditText;
 
 /**
@@ -14,7 +16,9 @@ import android.widget.EditText;
 
 public class EndEllipsisEditText extends EditText implements View.OnFocusChangeListener {
 
-    private String mOldStr;
+    private String mRealStr;
+    private String mFakeStr;
+    private boolean mIsFirstSet = true;
 
     public EndEllipsisEditText(Context context) {
         super(context);
@@ -31,31 +35,41 @@ public class EndEllipsisEditText extends EditText implements View.OnFocusChangeL
         init();
     }
 
-    private void init() {
+    public void init() {
         this.setOnFocusChangeListener(this);
-        mOldStr = getText() != null ? getText().toString() : "";
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    public EndEllipsisEditText(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
     }
 
     @Override
     public void onFocusChange(View v, boolean hasFocus) {
-        if(hasFocus){
-            setText(mOldStr);
-        }else{
-            mOldStr = getText()!=null?getText().toString():"";
-            if(getCharacterWidth(mOldStr,getTextSize())>getWidth()){
-                setText(getEllipsisStr(mOldStr));
-            }
+        setTheEditText(hasFocus);
+    }
+
+    @Override
+    protected boolean getDefaultEditable() {
+        return super.getDefaultEditable();
+    }
+
+    @Override
+    public void setText(CharSequence text, BufferType type) {
+        super.setText(text, type);
+        if (mIsFirstSet) {
+            mRealStr = getText() != null ? getText().toString() : "";
+            mIsFirstSet = false;
         }
     }
 
+    @Override
+    protected void onSelectionChanged(int selStart, int selEnd) {
+        super.onSelectionChanged(selStart, selEnd);
+    }
+
+    @Override
+    public Editable getText() {
+        return super.getText();
+    }
 
     private int getCharacterWidth(String text, float size) {
-        if (null == text || "".equals(text)){
+        if (null == text || "".equals(text)) {
             return 0;
         }
         Paint paint = new Paint();
@@ -64,15 +78,101 @@ public class EndEllipsisEditText extends EditText implements View.OnFocusChangeL
         return text_width;
     }
 
-    private String getEllipsisStr(String text){
+    public String getRealString(){
+        return mRealStr;
+    }
+
+    private void setTheEditText(boolean hasFocus) {
+        mFakeStr = getText() != null ? getText().toString() : "";
+        if (hasFocus) {
+
+            setText(mRealStr);
+        } else {
+            mRealStr = getText() != null ? getText().toString() : "";
+            if (getCharacterWidth(mRealStr, getTextSize()) > getWidth()) {
+                mFakeStr = getEllipsisStr(mRealStr);
+            } else {
+                mFakeStr = mRealStr;
+            }
+            setText(mFakeStr);
+        }
+    }
+
+    private String getEllipsisStr(String text) {
         String total = "";
-        for(int i=0;i<text.length();i++){
-            total =  text.substring(0,i);
-            if(getCharacterWidth(total, getTextSize())>getWidth()){
+        for (int i = 0; i < text.length(); i++) {
+            total = text.substring(0, i);
+            if (getCharacterWidth(total, getTextSize()) > getWidth()) {
                 break;
             }
         }
-        total = total.substring(0,total.length()-3)+"...";
+        total = total.substring(0, total.length() - 2) + "...";
         return total;
+    }
+
+    public void setListener(Activity activity, OnSoftKeyBoardChangeListener onSoftKeyBoardChangeListener) {
+        setOnSoftKeyBoardChangeListener(onSoftKeyBoardChangeListener);
+        SoftKeyBoardListener(activity);
+    }
+
+
+    private View rootView;//activity的根视图
+    private int rootViewVisibleHeight;//纪录根视图的显示高度
+    private OnSoftKeyBoardChangeListener onSoftKeyBoardChangeListener;
+
+    public void SoftKeyBoardListener(Activity activity) {
+        //获取activity的根视图
+        rootView = activity.getWindow().getDecorView();
+
+        //监听视图树中全局布局发生改变或者视图树中的某个视图的可视状态发生改变
+        rootView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                //获取当前根视图在屏幕上显示的大小
+                Rect r = new Rect();
+                rootView.getWindowVisibleDisplayFrame(r);
+
+                int visibleHeight = r.height();
+                System.out.println("" + visibleHeight);
+                if (rootViewVisibleHeight == 0) {
+                    rootViewVisibleHeight = visibleHeight;
+                    return;
+                }
+
+                //根视图显示高度没有变化，可以看作软键盘显示／隐藏状态没有改变
+                if (rootViewVisibleHeight == visibleHeight) {
+                    return;
+                }
+
+                //根视图显示高度变小超过200，可以看作软键盘显示了
+                if (rootViewVisibleHeight - visibleHeight > 200) {
+                    if (onSoftKeyBoardChangeListener != null) {
+                        onSoftKeyBoardChangeListener.keyBoardShow(rootViewVisibleHeight - visibleHeight);
+                    }
+                    rootViewVisibleHeight = visibleHeight;
+                    return;
+                }
+
+                //根视图显示高度变大超过200，可以看作软键盘隐藏了
+                if (visibleHeight - rootViewVisibleHeight > 200) {
+                    if (onSoftKeyBoardChangeListener != null) {
+                        onSoftKeyBoardChangeListener.keyBoardHide(visibleHeight - rootViewVisibleHeight);
+                    }
+                    rootViewVisibleHeight = visibleHeight;
+                    return;
+                }
+
+            }
+        });
+    }
+
+    private void setOnSoftKeyBoardChangeListener(OnSoftKeyBoardChangeListener onSoftKeyBoardChangeListener) {
+        this.onSoftKeyBoardChangeListener = onSoftKeyBoardChangeListener;
+    }
+
+    public interface OnSoftKeyBoardChangeListener {
+        void keyBoardShow(int height);
+
+        void keyBoardHide(int height);
     }
 }
--- app/src/main/java/com/example/zz/example/customcontrol/newedittext/NewEditTextActivity.java
+++ app/src/main/java/com/example/zz/example/customcontrol/newedittext/NewEditTextActivity.java
@@ -2,17 +2,58 @@ package com.example.zz.example.customcontrol.newedittext;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.method.KeyListener;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.zz.example.LogUtils;
 import com.example.zz.example.R;
 
 
 public class NewEditTextActivity extends AppCompatActivity {
+
+
+    private EditText normal_edit;
+    private View touch_view;
+    private EndEllipsisEditText endEllipsisEditText;
+    /**
+     * 软键盘弹出收起监听
+     */
+    private EndEllipsisEditText.OnSoftKeyBoardChangeListener onSoftKeyBoardChangeListener = new EndEllipsisEditText.OnSoftKeyBoardChangeListener() {
+        @Override
+        public void keyBoardShow(int height) {
+            endEllipsisEditText.requestFocus();
+            Toast.makeText(NewEditTextActivity.this, "键盘显示 高度 : " + height, Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void keyBoardHide(int height) {
+            endEllipsisEditText.clearFocus();
+            touch_view.requestFocus();
+            Toast.makeText(NewEditTextActivity.this, "键盘隐藏 高度 : " + height, Toast.LENGTH_LONG).show();
+        }
+    };
+
+    KeyListener keyListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         LogUtils.i("onCreate");
         setContentView(R.layout.edit_text_new);
+//        normal_edit = findViewById(R.id.normal_edit);
+//
+//        normal_edit.setText("这是普通edittext，如何加省略号");
+//        normal_edit.setEllipsize(TextUtils.TruncateAt.END);
 
+        endEllipsisEditText = findViewById(R.id.endEllipsisEditText);
+        endEllipsisEditText.setText("这是测试数据");
+        endEllipsisEditText.setListener(this, onSoftKeyBoardChangeListener);
+
+        touch_view = findViewById(R.id.touch_view);
     }
 }
