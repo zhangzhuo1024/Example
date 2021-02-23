eb418de28d75f517bbc185b25decb7281cc33860
zz <zhangzhuo1024@163.com>
2021-02-23 14:24:18 +0800

    1.解决打开webview时，直接调系统浏览器弹框 2.解决webview中进入二级目录后，按返回键没有返回直接退出webview问题

--- app/src/main/java/com/example/zz/example/network/NewsHtmlActivity.java
+++ app/src/main/java/com/example/zz/example/network/NewsHtmlActivity.java
@@ -1,20 +1,40 @@
 package com.example.zz.example.network;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.zz.example.R;
 
 public class NewsHtmlActivity extends AppCompatActivity {
-
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_html);
         String url = getIntent().getStringExtra("url");
-        WebView webView = findViewById(R.id.webView);
+        webView = findViewById(R.id.webView);
+
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setSupportMultipleWindows(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
         webView.loadUrl(url);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if(webView.canGoBack()){
+                webView.goBack();
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
