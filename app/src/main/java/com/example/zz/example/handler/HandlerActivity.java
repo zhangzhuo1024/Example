4a61310eb1d0bda167d2985ad66200d417e9346c
zz <zhangzhuo1024@163.com>
2021-04-23 11:12:54 +0800

    Handler实现读秒计时功能，用于测试界面卡顿

--- app/src/main/java/com/example/zz/example/handler/HandlerActivity.java
+++ app/src/main/java/com/example/zz/example/handler/HandlerActivity.java
@@ -1,15 +1,31 @@
 package com.example.zz.example.handler;
 
+import android.annotation.SuppressLint;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.zz.example.LogUtils;
 import com.example.zz.example.R;
 
 public class HandlerActivity extends AppCompatActivity {
+
+    private TextView mNum;
+    private int n = 0;
+    @SuppressLint("HandlerLeak") //放到mHandler在的方法外
+    private Handler mNumHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            mNum.setText("" + n);
+            n++;
+            mNumHandler.sendEmptyMessageDelayed(0, 100);
+        }
+    };
+
 
     private Handler mHandler = new Handler(new Handler.Callback() {
         @Override
@@ -50,6 +66,10 @@ public class HandlerActivity extends AppCompatActivity {
                 }
             }).start();
         });
+
+
+        mNum = findViewById(R.id.num);
+        mNumHandler.sendEmptyMessageDelayed(0, 100);
 
     }
 }
